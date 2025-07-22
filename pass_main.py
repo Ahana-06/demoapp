f0422ba35b97289805cddb7febf74a9453f1be3f
das-sounava <dsounava06@gmail.com>
2025-07-22 14:42:37 +0530

    0.0.0

--- pass_main.py
+++ pass_main.py
@@ -2,11 +2,11 @@ with open("password.txt","r") as p:
     pass_1=p.read()
     if(pass_1==""):
         pass_n1=input("enter a new password of six digit: ")
-        leg=len(pass_n1)
-        if(leg==6):
+        if(len(pass_n1)==6):
             pass_n2=input("enter confirmed password: ")
             if(pass_n1==pass_n2):
                 with open("password.txt","w") as p:
+                    print(f"password set to: {pass_n1}" )
                     p.write(pass_n1)
             elif(pass_n1!=pass_n2):
                 print("enter a valid password")
@@ -15,7 +15,6 @@ with open("password.txt","r") as p:
     elif(pass_1!=""):
         pass_lg=input("enter login password: ")
         if(pass_1==pass_lg):
-            print("successfully loged in")
+            print("successfully logged in")
         elif(pass_1!=pass_lg):
             print("enter a valid password")
-
